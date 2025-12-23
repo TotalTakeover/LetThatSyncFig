2c1ede088ca0cd0b652710bec7e961bb5a35e8a3
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-12-23 12:42:30 -0700

    Allow keybinds to sync to config
    "For what?" - Irri

--- LetThatSyncFig.lua
+++ LetThatSyncFig.lua
@@ -9,7 +9,7 @@
 --          \|__|  \|_______|    \|__|  \|__|\|__|\|_______|
 --
 -- Special thanks: Grandpa Scout & Pool
--- Version: 1.0.3
+-- Version: 1.0.4
 
 -- Config setup
 config:name("NameHere")
@@ -56,11 +56,50 @@ end
 -- Host only instructions
 if not host:isHost() then return sync end
 
+-- Create keybinds table
+local keys = {}
+
+-- Stores keybind in table while creating for syncing config with current state
+function sync.keybind(keybind, configName)
+	
+	-- Attach bind
+	keybind:key(config:load(configName) or keybind:getKey())
+	
+	-- Store keybind 
+	keys[keybind] = {
+		config = configName,
+		key = keybind:getKey()
+	}
+	
+end
+
 -- Sync on tick
 events.TICK:register(function()
 	
+	-- Syncs variables
 	if world.getTime() % 200 == 0 then
 		pings.syncVars(table.unpack(sync))
+	end
+	
+	-- Syncs keybinds to configs
+	if host:getScreen() == "org.figuramc.figura.gui.screens.KeybindScreen" then
+		for k, v in pairs(keys) do
+			
+			-- Get current key
+			local key = k:getKey()
+			
+			-- Compare keys
+			if v.key ~= key then
+				
+				-- Save to config
+				config:save(v.config, key)
+				
+				-- Store data
+				v.key = key
+				
+			end
+			
+		end
 	end
 	
 end, "tickSync")
