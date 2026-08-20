f71528c6810b04bb4437b6de78fe87398553e4f9
Total <39064108+TotalTakeover@users.noreply.github.com>
2026-08-20 00:08:47 -0700

    VSCode LuaLS Documentation + More!
    Holy shit I'm a real dev now!
    - Added documentation to the library for better control.
      - Obv I will tweak this later on. Point is, this is far better than it was before
    - Renamed variables/symbols
    - Rewrote comments
    - `addFunc` -> `addFuncs` now takes multiple functions and returns them, no longer returning self for chaining.
    - `removeFunc` -> `removeFuncs` now removes multiplue functions, but still returns self for chaining.

--- LetThatSyncFig.lua
+++ LetThatSyncFig.lua
@@ -9,35 +9,65 @@
 --          \|__|  \|_______|    \|__|  \|__|\|__|\|_______|
 --
 -- Special thanks: Grandpa Scout, Pool & Mangodev
--- Version: 1.1.7
+-- Version: 1.1.8
 
--- Create API
+-- An API for handling the creation of Sync Objects.
+---@class SyncAPI
 local syncAPI = {}
 
--- Syncs table
+-- A sync object.
+---@class SyncObject
+-- The unique id of a sync object.
+---@field id string
+-- The numerical id of a sync object.
+---@field nid integer
+-- The previous state of a sync object's value.
+---@field prev any
+-- The current state of a sync object's value.
+---@field curr any
+-- A table holding functions for a sync object to preform.
+---@field funcs table
+-- A countdown for how many ticks before an object's ping is sent.
+---@field countdown integer
+local syncObject = {}
+
+-- A table that holds the sync objects.
 local syncs = {}
 
--- Interal sync data
-local syncInternal = {}
-
--- Meta table setup
+-- The metatable for sync objects.
 local syncMeta = {
-	__index = syncInternal,
+	__index = syncObject,
 	__type = "SyncObject"
 }
 
--- Type checker that errors if type isnt what's needed
-local errorOverride = false -- Unique ID error message likes to fight the typeCheck error message for some reason, this prevents that
+-- Unique ID error message likes to fight the `typeCheck` error message for some reason; this prevents that.
+local errorOverride = false
+
+-- Type checker that errors if type isnt what's needed.
+---@param value any #
+-- Value that is checked to ensure it is matching the provided type.
+---@param typeStr string #
+-- Type the `value` is checked against.
 local function typeCheck(value, typeStr)
-	
 	if type(value) ~= typeStr then
 		errorOverride = true
 		error("\n\n§6Argument must be a "..typeStr.."!\n§c", 3)
 	end
-	
 end
 
--- Create a sync object
+-- Creates a sync object.
+---@param id string #
+-- The unique id of a sync object.  
+-- \>>> **MUST BE UNIQUE** <<<  
+-- Sync objects use numerical ids to send lightweight pings to other clients.  
+-- The numerical ids are generated using the id provided, ensuring the number ids are the same across clients.  
+-- It's also helpful for debugging :)
+---@param ... any #
+-- The default value of a sync object.  
+-- You may send as many values as you would like; the sync object will only use the first value thats NOT `nil`.  
+-- This is useful if you would like an optional default that doesnt always occur, and a backup thats guaranteed.  
+-- If all values are `nil`, the sync object's value will be `nil`.
+---@nodiscard
 function syncAPI.new(id, ...)
 	
 	-- Check if the id is a string
@@ -53,7 +83,7 @@ function syncAPI.new(id, ...)
 	end
 	
 	-- Determine which value should be applied, checking for nil before returning
-	local result
+	local result = nil
 	for i = 1, select("#", ...) do
 		local value = select(i, ...)
 		if value ~= nil then
@@ -65,9 +95,9 @@ function syncAPI.new(id, ...)
 	local obj = setmetatable(
 		{
 			id = id,
-			funcs = {},
 			prev = result,
-			curr = result
+			curr = result,
+			funcs = {}
 		},
 		syncMeta
 	)
@@ -80,7 +110,7 @@ function syncAPI.new(id, ...)
 	
 end
 
--- Sorts table deterministically
+-- This entity init event sorts the sync table deterministically across clients, making sure the ids match.
 function events.ENTITY_INIT()
 	
 	-- Sorts table alphabetically
@@ -95,7 +125,11 @@ function events.ENTITY_INIT()
 	
 end
 
--- Updates sync values
+-- Preforms the actual update to a sync object.
+---@param obj SyncObject #
+-- The sync object the update is being preformed on.
+---@param value any #
+-- The value a sync object is being updated to.
 local function updateValues(obj, value)
 	
 	-- Update current value
@@ -117,34 +151,32 @@ local function updateValues(obj, value)
 	
 end
 
--- Sync variable via ping
-function pings.sendSyncUpdate(key, value)
-	
-	-- Find sync object
-	local obj = syncs[key]
-	
-	-- Update values
-	updateValues(obj, value)
-	
+-- Sends a ping to other clients to update a sync object using its number id.
+---@param nid integer #
+-- The id of a sync object.
+---@param value any #
+-- The value a sync object is being updated to.
+function pings.sendSyncUpdate(nid, value)
+	updateValues(syncs[nid], value)
 end
 
--- Sync ALL variables via ping
+-- Sends a ping to other clients to update ALL sync objects to their current values.
+---@param ... any #
+-- The values of the sync objects, in order of number id.
 function pings.sendSyncUpdateAll(...)
-	
 	for i, value in ipairs({...}) do
-		
-		-- Find sync object
-		local obj = syncs[i]
-		
-		-- Update values
-		updateValues(obj, value)
-		
+		updateValues(syncs[i], value)
 	end
-	
 end
 
--- Update a sync object
-function syncInternal:update(value, buffer)
+-- Update a sync object with a value.  
+-- If the value is different from its previous, a ping is sent to other clients.
+---@param value any #
+-- The value a sync object is being updated to.
+---@param buffer? number #
+-- How many ticks the update will wait before sending the ping to other clients.  
+-- This is useful for preventing ping spam, usually caused by rapid clicking and scroll wheels.
+function syncObject:update(value, buffer)
 	
 	-- Check if change occured, and send ping
 	-- Prevents spam caused by user
@@ -161,7 +193,7 @@ function syncInternal:update(value, buffer)
 			self.countdown = buffer
 			updateValues(self, value)
 			
-			-- Return object
+			-- Return object early
 			return self
 			
 		end
@@ -176,36 +208,52 @@ function syncInternal:update(value, buffer)
 	
 end
 
--- Apply a function
-function syncInternal:addFunc(func)
+-- Add functions to a sync object that will be preformed when its value is updated.
+---@param ... function #
+-- The functions that will be added to a sync object.
+function syncObject:addFuncs(...)
 	
-	-- Checks if function is actually a function
-	typeCheck(func, "function")
+	-- Iterate through varargs
+	for _, func in ipairs({...}) do
+		
+		-- Checks if function is actually a function
+		typeCheck(func, "function")
+		
+		-- Apply function to sync
+		self.funcs[func] = func
+		
+	end
 	
-	-- Apply function to sync
-	self.funcs[func] = func
-	
-	-- Return object and function (incase you need it)
-	return self, func
+	-- Return functions (incase you need them)
+	return ...
 	
 end
 
--- Remove a function
-function syncInternal:removeFunc(func)
+-- Remove functions from a sync object (added by `addFuncs()`).
+---@param ... function #
+-- The functions that will be removed from a sync object.
+function syncObject:removeFuncs(...)
 	
-	-- Checks if function is actually a function
-	typeCheck(func, "function")
-	
-	-- Remove function from sync
-	self.funcs[func] = nil
+	-- Iterate through varargs
+	for _, func in ipairs({...}) do
+		
+		-- Checks if function is actually a function
+		typeCheck(func, "function")
+		
+		-- Remove function from sync
+		self.funcs[func] = nil
+		
+	end
 	
 	-- Return object
 	return self
 	
 end
 
--- Apply a config key
-function syncInternal:config(cfgName)
+-- Apply a config key to a sync object, so its value is saved across sessions.
+---@param cfgName? string #
+-- An optional custom config key. If none is provided, the object's ID is used.
+function syncObject:config(cfgName)
 	
 	-- Kill function if not host
 	if not host:isHost() then return self end
@@ -234,18 +282,22 @@ end
 -- Host only instructions
 if not host:isHost() then return syncAPI end
 
--- Sync on tick
+-- The previous tick.
 local _tick = 0
+
+-- This tick function updates all sync objects across all clients every 10 seconds.
 events.TICK:register(function()
 	
-	-- Get time
+	-- Get world time
 	local tick = world.getTime()
 	
 	-- Sync variables
 	if tick % 200 == 0 and tick ~= _tick then
 		
-		-- Gather values
+		-- Sync objects values.
 		local syncTables = {} 
+		
+		-- Gather values
 		for i, obj in ipairs(syncs) do
 			syncTables[i] = obj.curr
 		end
@@ -263,7 +315,7 @@ events.TICK:register(function()
 	for _, obj in ipairs(syncs) do
 		if obj.countdown then
 			
-			-- Decrement countdown
+			-- How many ticks left before ping.
 			obj.countdown = math.max(obj.countdown - 1, 0)
 			
 			-- If countdown reaches 0, send ping
